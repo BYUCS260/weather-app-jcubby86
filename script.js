87fde2a65609425ba064ae2c7f46cee0047deca8
Jacob Bastian <jcubby86@hotmail.com>
2022-02-12 22:20:22 -0700

    Update styling and javascript

--- script.js
+++ script.js
@@ -5,52 +5,56 @@ function addCurrentWeather(json) {
   weatherResults.textContent = '';
 
   weatherResults.appendChild(document.createElement("hr"));
-
-  const inCity = weatherResults.appendChild(document.createElement("h2"));
-  inCity.textContent = 'Weather in ' + json.name;
+  weatherResults.appendChild(document.createElement("h2")).textContent = 'Weather in ' + json.name;
 
   const item = weatherResults.appendChild(document.createElement("div"));
   item.setAttribute("class", "item");
 
-  const temp = item.appendChild(document.createElement("h4"));
-  temp.innerHTML = "Temperature: " + json.main.temp + ' &deg;F';
+  item.appendChild(document.createElement("h4")).innerHTML = "Temperature: " + Math.round(json.main.temp) + ' &deg;F';
+  item.appendChild(document.createElement("h4")).innerHTML = "Feels like: " + Math.round(json.main.feels_like) + ' &deg;F';
+  item.appendChild(document.createElement("h4")).innerHTML = "H:" + Math.round(json.main.temp_max) + '&deg; L:' + Math.round(json.main.temp_min) + '&deg;';
+  item.appendChild(document.createElement("h4")).innerHTML = "Humidity: " + json.main.humidity + "%";
+  item.appendChild(document.createElement("h4")).innerHTML = "Sunrise: " + moment.unix(json.sys.sunrise).format('h:mma') + " - Sunset: " + moment.unix(json.sys.sunset).format('h:mma');
 
   let results = '';
   for (let i = 0; i < json.weather.length; i++) {
-    const image = item.appendChild(document.createElement("img"));
-    image.setAttribute('src', 'http://openweathermap.org/img/w/' + json.weather[i].icon + '.png');
+    item.appendChild(document.createElement("img")).setAttribute('src', 'http://openweathermap.org/img/w/' + json.weather[i].icon + '.png');
 
-    results += json.weather[i].description
+    results += json.weather[i].main + ' - ' + json.weather[i].description
     if (i !== json.weather.length - 1) {
       results += ", ";
     }
   }
 
-  const p = item.appendChild(document.createElement("p"));
-  p.textContent = results;
+  item.appendChild(document.createElement("h6")).textContent = results;
 }
 
-function addForecast(json){
+function addForecast(json) {
   const forecastResults = document.getElementById("forecastResults");
   forecastResults.textContent = '';
 
-  const city = forecastResults.appendChild(document.createElement("h2"));
-  city.textContent = '15-day Forecast';
+  forecastResults.appendChild(document.createElement("h2")).textContent = '5-day Forecast';
 
-  const section = forecastResults.appendChild(document.createElement("div"));
-  section.setAttribute("class", "section");
-  for (let i=0; i < json.list.length; i++) {
+  let curr;
+  let section;
+  for (let i = 0; i < json.list.length; i++) {
+    const dateString = moment(json.list[i].dt_txt).format('dddd, MMMM Do');
+    if (i === 0 || dateString !== curr) {
+      curr = dateString;
+      forecastResults.appendChild(document.createElement("h3")).textContent = curr;
+      section = forecastResults.appendChild(document.createElement("div"));
+      section.setAttribute("class", "section day");
+    }
+
     const item = section.appendChild(document.createElement("div"));
     item.setAttribute("class", "item");
 
-    const h2 = item.appendChild(document.createElement("h3"));
-    h2.textContent = moment(json.list[i].dt_txt).format('ddd, MMM D, h:mma');
+    item.appendChild(document.createElement("h3")).textContent = moment(json.list[i].dt_txt).format('h:mma');
+    item.appendChild(document.createElement("h4")).innerHTML = "Temp: " + Math.round(json.list[i].main.temp) + ' &deg;F';
+    item.appendChild(document.createElement("h4")).innerHTML = "Feels like: " + Math.round(json.list[i].main.feels_like) + ' &deg;F';
+    item.appendChild(document.createElement("h4")).innerHTML = "Humidity: " + json.list[i].main.humidity + "%";
+    item.appendChild(document.createElement("img")).setAttribute("src", 'http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png');
 
-    const p = item.appendChild(document.createElement("h5"));
-    p.innerHTML = json.list[i].main.temp + ' &deg;F';
-
-    const img = item.appendChild(document.createElement("img"));
-    img.setAttribute("src", 'http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png');
   }
 
 }
@@ -74,9 +78,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",US&units=imperial" + "&APPID=" + APIKEY;
     fetch(forecastURL)
-      .then(function(response) {
+      .then(function (response) {
         return response.json();
-      }).then(function(json) {
+      }).then(function (json) {
         console.log(json);
         addForecast(json);
       });
